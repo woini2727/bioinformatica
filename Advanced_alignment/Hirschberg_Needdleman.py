749d3b08dd8bf0642d043c4d68f6334b27c7684e
woini2727 <salvador.woini@gmail.com>
2019-11-13 12:06:32 -0300

    score calculation

--- Advanced_alignment/Hirschberg_Needdleman.py
+++ Advanced_alignment/Hirschberg_Needdleman.py
@@ -14,9 +14,11 @@ def main():
         matrix[i][1] = 0
         cont -= 1
 
-    ##recorro la matriz de a 2 columnas hasta la mitad y me paro en el mayor de dicha columna mitad
-    for i in range (n//2):
-
+    while n >= 2:
+        # recorro la matriz de a 2 columnas hasta la mitad y me paro en el mayor de dicha columna mitad
+        n = n//2
+        contt=0
+        m-=contt
         for j in range(m):
             if j == 0 :
                 matrix[j][1] = matrix[j][0] - 1
@@ -24,9 +26,18 @@ def main():
                 diag = matrix[j-1][0]
                 izq = matrix[j][0] - 1
                 sup = matrix[j-1][1] - 1
-                matrix[j][1] = max(diag,izq,sup)
+                    #matrix[j][1] = max(diag,izq,sup)
+                print(ref[0] ,seq[j-1])
+                if ref[0] == seq[j-1]:
+                    matrix[j][1] = max(diag + 1, sup, izq)
+                else:
+                    matrix[j][1] = max(diag - 1, sup, izq)
         print(matrix)
-    ## una vez que tengo el mayor divido prefix[] y sink[] y empiezo a buscar los mayores de cada mitad recursivamente
+        n=0
+        # inverse
+
+    # una vez que tengo el mayor divido prefix[] y sink[] y empiezo a buscar los mayores de cada mitad recursivamente
+
 
 if __name__ == '__main__':
     main()
