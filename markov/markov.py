32a1410eb8375e1de2ab5e9ab61290bdf4f376a8
woini2727 <salvador.woini@gmail.com>
2019-11-17 20:46:37 -0300

    mhmm

---
+++ markov/markov.py
@@ -0,0 +1,11 @@
+
+def markov():
+    symbols = ["T","H"]
+    states = ["F","B"]
+    matrizA = [[0.50,0.75],[0.5,0.25]]
+    matrizE = [[0.7,0.3],[0.3,0.7]]
+
+
+
+if __name__ == '__main__':
+    markov()
