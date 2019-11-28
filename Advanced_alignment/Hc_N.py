b0e4b53599e75b2bdd6c822867fe0c8b7c9ee799
woini2727 <salvador.woini@gmail.com>
2019-11-28 17:58:19 -0300

    Update Hc_N.py

--- Advanced_alignment/Hc_N.py
+++ Advanced_alignment/Hc_N.py
@@ -1,53 +1,82 @@
-import operator
+import numpy as np
 
-def main(seq,ref):
-    z = ""
-    w = ""
-    print(hirschberg(seq,ref,z,w))
+def Ins(x):
+  return -2
 
+def Del(x):
+  return -2
 
-def score(seq,ref):
-    score = [[0] * (len(seq)+1) for i in range(2)]
-    for j in range(1,len(seq)+1):
-        score[0][j] = score[0][j-1] -1
-    for i in range(0,2):
-        score[i][0] = score[i-1][0] -1
-        for j in range(1,len(seq)+1):
-            similarity = 1 if ref[i - 1] == seq[j - 1] else -1
-            diag = score[i-1][j-1] + similarity
-            izq = score[i][j-1] -1
-            der = score[i-1][j] -1
-            score[i][j] = max(diag,izq,der)
-        score[0][:] = score[1][:]
-    lastline = []
-    for j in range(len(seq)+1):
-        lastline.append(score[1][j])
-    return lastline
+def Sub(x, y):
+  return 2 if x == y else -1
 
-def hirschberg(seq,ref,z,w):
-    if len(ref) == 0:
-      for i in range(len(seq)):
-        z = z + "-"
-        w = w + seq[i]
-    elif len(seq) == 0:
-        for i in range(len(ref)):
-            z += ref[i]
-            w += "-"
-    elif len(ref) == 1 or len(seq) == 1:
-        #print(ref, seq)
-        l =list(needlemanWunsch(ref,seq))
-        print(l)
+def NWScore(X, Y):
+  score = [[0] * (1 + len(Y)) for _ in range(0, len(X) + 1)]
+ 
+  for j in range(1, len(Y) + 1):
+    score[0][j] = score[0][j-1] + Ins(Y[j-1])
+ 
+  for i in range(1, len(X) + 1): #init array
+      score[i][0] = score[i-1][0] + Del(X[i-1])
+      for j in range(1, len(Y) + 1):
+        scoreSub = score[i-1][j-1] + Sub(X[i-1], Y[j-1])
+        scoreDel = score[i-1][j] + Del(X[i-1])
+        scoreIns = score[i][j-1] + Ins(Y[j-1])
+        score[i][j] = max(scoreSub, scoreDel, scoreIns)
+
+  return score[-1]
+
+def rev(s):
+  return s[::-1]
+
+def Diagonal(n1,n2,pt):
+    if(n1 == n2):
+        return pt['MATCH']
     else:
-        refmid = len(ref) // 2  ##siempre a la mitad
-        rev = ref[::-1]
-        scoreizq = score(seq, ref[:refmid+1])
-        scoreder = score(seq[::-1],rev[refmid:])
-        seqmid = find_max_pos(scoreizq,reversed(scoreder))
-        z,w= hirschberg(seq[ :seqmid],ref[:refmid],z,w), hirschberg(seq[seqmid:],ref[refmid:],z,w)
-        # seqmidiz = (len(seq) + 1) // 2
-        # seqmidder = len(seq) // 2
-        #z,w= hirschberg(seq[ :seqmidder],ref[:refmid],z,w), hirschberg(seq[seqmidiz-1:],ref[refmid:],z,w)
-    return (z,w)
+        return pt['MISMATCH']
+
+#------------------------------------------------------------  
+#This function gets the optional elements of the aligment matrix and returns the elements for the pointers matrix.
+def Pointers(di,ho,ve):
+
+    pointer = max(di,ho,ve) #based on python default maximum(return the first element).
+
+    if(di == pointer):
+        return 'D'
+    elif(ho == pointer):
+        return 'H'
+    else:
+         return 'V'  
+
+def NeedlemanWunsch(s1,s2,match = 1,mismatch = -1, gap = -2):
+    penalty = {'MATCH': match, 'MISMATCH': mismatch, 'GAP': gap} #A dictionary for all the penalty valuse.
+    n = len(s1) + 1 #The dimension of the matrix columns.
+    m = len(s2) + 1 #The dimension of the matrix rows.
+    al_mat = np.zeros((m,n),dtype = int) #Initializes the alighment matrix with zeros.
+    p_mat = np.zeros((m,n),dtype = str) #Initializes the alighment matrix with zeros.
+    #Scans all the first rows element in the matrix and fill it with "gap penalty"
+    for i in range(m):
+        al_mat[i][0] = penalty['GAP'] * i
+        p_mat[i][0] = 'V'
+    #Scans all the first columns element in the matrix and fill it with "gap penalty"
+    for j in range (n):
+        al_mat[0][j] = penalty['GAP'] * j
+        p_mat [0][j] = 'H'
+    #Fill the matrix with the correct values.
+
+    p_mat [0][0] = 0 #Return the first element of the pointer matrix back to 0.
+    for i in range(1,m):
+        for j in range(1,n):
+            di = al_mat[i-1][j-1] + Diagonal(s1[j-1],s2[i-1],penalty) #The value for match/mismatch -  diagonal.
+            ho = al_mat[i][j-1] + penalty['GAP'] #The value for gap - horizontal.(from the left cell)
+            ve = al_mat[i-1][j] + penalty['GAP'] #The value for gap - vertical.(from the upper cell)
+            al_mat[i][j] = max(di,ho,ve) #Fill the matrix with the maximal value.(based on the python default maximum)
+            p_mat[i][j] = Pointers(di,ho,ve)
+
+    ret = []
+    for i in range(m):
+      ret.append(max([ al_mat[i][j] for j in range(n) ]))
+
+    return ret
 
 def find_max_pos(score1,score2):
     pts = [s1 + s2 for s1, s2 in zip(score1, score2)]
@@ -55,40 +84,46 @@ def find_max_pos(score1,score2):
     pos = pts.index(maximo)
     return pos
 
-def needlemanWunsch(ref,seq):
-    dp_table = [[0] * (len(seq)+1) for i in range(len(ref)+1)]
-    for i in range(1, len(ref)+1):
-        for j in range(1, len(seq)+1):
-            similarity = 1 if ref[i-1] == seq[j-1] else -1
-            gap_penalty = -1
-            dp_table[i][j] = max(
-                    dp_table[i-1][j-1] + similarity,
-                    dp_table[i-1][j] + gap_penalty,
-                    dp_table[i][j-1] + gap_penalty
-                    )
-    i, j = len(ref), len(seq)
-    while i != 0 and j != 0:
-        yield (i,j)
-        #print(ref[i-1],seq[j-1])
-        similarity = 1 if ref[i-1] == seq[j-1] else -1
-        gap_penalty = -1
-        a, b, c = dp_table[i-1][j-1] + similarity, dp_table[i-1][j] + gap_penalty, dp_table[i][j-1] + gap_penalty
-        m = max(a,b,c)
-        if m == a:
-            i -= 1
-            j -= 1
-        elif m == b:
-            i -= 1
-        elif m == c:
-            j -= 1
-    yield (i,j)
+def Hirschberg(X,Y, ret = []):
+  #print(X, Y)
+  ret.append((X, Y))
 
-if __name__ == '__main__':
-    ref = "AGTACGCA"
-    seq = "TATGC"
-    """for nums in needlemanWunsch(ref,seq):
-       i,j=nums
-       print(nums)"""
+  Z = ""
+  W = ""
+  if len(X) == 0:
+    for y in Y:
+      Z += '-'
+      W += y
+  elif len(Y) == 0:
+    for x in X:
+      Z += x
+      W += '-'
+  elif len(X) == 1 or len(Y) == 1:
+    Z, W = NeedlemanWunsch(X, Y)
+  else:
+    xmid = len(X)//2
+    Xl = X[:xmid]
+    Xr = X[xmid:]
 
-    main(seq,ref)
+    ScoreL = NWScore(Xl, Y)
+    ScoreR = NWScore(rev(Xr), rev(Y))
+    ymid = find_max_pos(ScoreL, rev(ScoreR))
+
+    Yl = Y[:ymid]
+    Yr = Y[ymid:]
+
+    Z, W = Hirschberg(Xr, Yr, ret)
+    z, w = Hirschberg(Xl, Yl, ret)
+
+    if(type(w) == str and type(z) == str):
+      return (w, z)
+   
+    Z += z
+    W += w
+
+  return (Z, W)
+
+ret = []
+print(Hirschberg("AGTACGCA", "TATGC", ret))
+print(ret)
 
